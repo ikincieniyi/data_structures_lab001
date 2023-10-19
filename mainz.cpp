b044494858137e09a84418aad52ac42daaae5ab7
ikincieniyi <zekeriyaorman1903@gmail.com>
2023-10-19 17:44:45 +0300

    i made a design for output. It looks prettier

--- mainz.cpp
+++ mainz.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 void readCharFromFile(std::ifstream& charFile, char** charArr, int row, int column);
+void showBlobs(char** charArr, int row, int column);
 
 int main(void)
 {
@@ -45,15 +46,16 @@ int main(void)
     
     inputFile.close(); //closing the input file
 
+    showBlobs(blobs, row, column);
 
-    for(int i = 0; i < row; i++)
+/*     for(int i = 0; i < row; i++)
     {
         for(int j = 0; j < column; j++)
         {
             std::cout << blobs[i][j] << " ";
         }
         std::cout << std::endl;
-    }
+    } */
     
     for(int i = 0; i < row; i++)
     {
@@ -81,4 +83,43 @@ void readCharFromFile(std::ifstream& charFile, char** charArr, int row, int colu
     {
         std::cerr << "File ended prematurely" << std::endl;
     }
+}
+
+void addLine(int width)
+{
+    std::cout << " +";
+    for(int i = 0; i < width; i++)
+    {
+        std::cout << "-";
+    }
+    std::cout << "+ " << std::endl;
+}
+
+void addLineNum(int width)
+{
+    std::cout << "  "; //2blank space for the table design
+    for(int i = 0; i < width; i++)
+    {
+        std::cout << i % 10 << " "; //printing the last digit of column number so it can only took 1 space
+    }
+    std::cout << std::endl;
+}
+
+void showBlobs(char** charArr, int row, int column)
+{
+    addLineNum(column);
+    addLine(2 * column - 1);
+
+    for(int i = 0; i < row; i++)
+    {
+        std::cout << i << "|";
+        for(int j = 0; j < column; j++)
+        {
+            std::cout << charArr[i][j] << " ";
+        }
+        std::cout << "\b|" << i << std::endl;
+    }
+
+    addLine(2 * column - 1);
+    addLineNum(column);
 }
