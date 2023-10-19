991389bab2e57e3084562361c294199c28730ce3
ikincieniyi <zekeriyaorman1903@gmail.com>
2023-10-19 18:06:07 +0300

    added some comment lines

--- mainz.cpp
+++ mainz.cpp
@@ -31,32 +31,13 @@ int main(void)
         blobs[i] = new char[column];
     }
 
-    readCharFromFile(inputFile, blobs, row, column);
-/*     //reading the input from the file
-    std::string blobStr; //this string will carry the data from given file to the blobs array
-    int rowCounter = 0;
-    while(std::getline(inputFile, blobStr) && rowCounter < row)
-    {
-        for(int i = 0; i < column; i++)
-        {
-            blobs[rowCounter][i] = blobStr[i];
-        }
-        rowCounter++;
-    } */
-    
+    readCharFromFile(inputFile, blobs, row, column); //reads the cars from inputFile file and add them into the blobs array
+
     inputFile.close(); //closing the input file
 
-    showBlobs(blobs, row, column);
+    showBlobs(blobs, row, column); //shows the blobs with pretty format
 
-/*     for(int i = 0; i < row; i++)
-    {
-        for(int j = 0; j < column; j++)
-        {
-            std::cout << blobs[i][j] << " ";
-        }
-        std::cout << std::endl;
-    } */
-    
+    //releasing the memory
     for(int i = 0; i < row; i++)
     {
         delete[] blobs[i];
@@ -70,22 +51,25 @@ void readCharFromFile(std::ifstream& charFile, char** charArr, int row, int colu
     //reading the input from the file
     std::string blobStr; //this string will carry the data from given file to the blobs array
     int rowCounter = 0;
-    while(std::getline(charFile, blobStr) && rowCounter < row)
+    while(std::getline(charFile, blobStr) && rowCounter < row) //reading the file line to line
     {
-        for(int i = 0; i < column; i++)
+        for(int i = 0; i < column; i++) //after reading each line carries the chars in that line into to array
         {
             charArr[rowCounter][i] = blobStr[i];
         }
         rowCounter++;
     }
 
-    if(rowCounter < row)
+    if(rowCounter < row) //if reads less char from the expected, shows an error
     {
-        std::cerr << "File ended prematurely" << std::endl;
+        std::cout << "########################" << std::endl;
+        std::cerr << "#File ended prematurely#" << std::endl;
+        std::cout << "########################" << std::endl;
+        std::cerr << "expected row was: " << row << " but there was " << rowCounter << " row" << std::endl;
     }
 }
 
-void addLine(int width)
+void addLine(int width) // adds line between line numbers and table
 {
     std::cout << " +";
     for(int i = 0; i < width; i++)
@@ -95,7 +79,7 @@ void addLine(int width)
     std::cout << "+ " << std::endl;
 }
 
-void addLineNum(int width)
+void addLineNum(int width) // adds line number to the table
 {
     std::cout << "  "; //2blank space for the table design
     for(int i = 0; i < width; i++)
@@ -107,8 +91,8 @@ void addLineNum(int width)
 
 void showBlobs(char** charArr, int row, int column)
 {
-    addLineNum(column);
-    addLine(2 * column - 1);
+    addLineNum(column); // adds line number to the table
+    addLine(2 * column - 1); // adds line between line numbers and table. it adds 2*column-1 char because I added 1 space between rows.
 
     for(int i = 0; i < row; i++)
     {
@@ -117,7 +101,7 @@ void showBlobs(char** charArr, int row, int column)
         {
             std::cout << charArr[i][j] << " ";
         }
-        std::cout << "\b|" << i << std::endl;
+        std::cout << "\b|" << i << std::endl; // '\b' is for erasing the last space after last member of the each row
     }
 
     addLine(2 * column - 1);
